83eefc77aa45d22daf8517f9326f803c1f2ed499
Rameshwar Navathar <rameshwarjayhanuman99@gmail.com>
2024-08-07 13:56:24 -1200

    new update 0.3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const route=require("./route/route");
-const app=express();
-app.use(express.json());
+const express= require("express")
+const mongoose= require("mongoose")
+require("dotenv").config()
+
+const route= require("./route/route")
+const app =express();
+const PORT =process.env.PORT ||4000
 
 
-mongoose.set('strictQuery', false)
-mongoose.connect("mongodb+srv://RameshwarJay:RUlNC8QtlA8A91jX@rameshwarnavathar.dujri1m.mongodb.net/")
 
-.then(() => {console.log("MongoDB is connected! 😎")})
-.catch((errors) => {console.log(errors.message)})
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>console.log('Data base is connected!!'))
+.catch((err)=>console.log(err))
 
-app.use("/", route);
 
-app.listen(4000, function () {
-console.log("Server start successfully!",+ 4000)
-})
+app.use('/',route)
+
+app.listen(PORT ,()=>console.log(`Server is start on http://localhost:${PORT}`))
